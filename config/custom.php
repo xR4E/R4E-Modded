000830832f34d989039fbeac9ff9862aed215779
shlandturtle <42299553+shlandturtle@users.noreply.github.com>
2020-02-21 11:17:21 +0000

    Update donations blade

---
+++ config/custom.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ *
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Donations goal
+    |--------------------------------------------------------------------------
+    |
+    | Monthly donation goal
+    |
+    */
+
+    'donation-goal' => '1',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Donations currency
+    |--------------------------------------------------------------------------
+    |
+    | In what unit is donations mentioned
+    |
+    */
+
+    'donation-unit' => 'Unit',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Donations amount
+    |--------------------------------------------------------------------------
+    |
+    | Current months donations so far, without unit
+    |
+    */
+    'donation-amount' => '0',
+
+];
--- resources/views/blocks/donations.blade.php
+++ resources/views/blocks/donations.blade.php
@@ -1,13 +1,20 @@
 <div class="col-md-10 col-sm-10 col-md-offset-1">
     <div class="clearfix visible-sm-block"></div>
-        <div class="panel panel-chat shoutbox">
+        <div class="panel panel-chat">
             <div class="panel-heading">
                 <h4>@lang('blocks.donation-title')</h4>
             </div>
             <div class="panel-body">
-                <h3 style="margin-top: 10px;">Goal: {{ config("r4e.donation-goal", '0') }} {{ config("r4e.donation-unit", 'Unit') }}</h3>
-                <div class="progress"><div role="progressbar" aria-valuenow="$data" aria-valuemin="0" aria-valuemax="{{ config("r4e.donation-goal", '0') }}" class="progress-bar progress-bar-striped active" style="width:{{ config("r4e.donation-amount", '0') }}%;">
-                    {{ config("r4e.donation-amount", '0') }}{{ config("r4e.donation-unit", 'Units') }}
+            @php
+            $donation_progress = (config("custom.donation-amount", '0') / config("custom.donation-goal", '1') ) * 100;
+            $donation_progress = min(round($donation_progress, 2), 100);
+            @endphp
+                <h3 style="margin-top: 5px;">Goal: {{ config("custom.donation-goal", '0') }} {{ config("custom.donation-unit", 'Unit') }}</h3>
+                <div class="progress" style="white-space: nowrap;">
+                <div class="progress-bar progress-bar-striped active" role="progressbar"
+                aria-valuenow="{{ $donation_progress }}" aria-valuemin="0"
+                aria-valuemax="100" style="width: {{ $donation_progress }}%;">
+                {{ round(config("custom.donation-amount", '0'), 2) }} {{ config("custom.donation-unit", 'Units') }}
                 </div>
             </div>
         </div>
